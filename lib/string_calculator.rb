b13f0936e114908f7da65c08699f151d6819d118
vivek negi <v_111negi@yahoo.com>
2024-06-10 00:02:16 +0530

    if input string contains \n & negative numbers + positive numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -5,5 +5,17 @@ class StringCalculator
 	## calculating sum
 	def add(str)
 		return 0 if str.empty?
+
+		if str.start_with?("//")
+			arr = str.gsub("\n", "").split(str[2]).map(&:to_i)
+		else
+			arr = str.gsub("\n", ",").split(",").map(&:to_i)
+		end
+
+		unless arr.select(&:negative?).empty?
+			raise "negative numbers not allowed #{arr.select(&:negative?) * ','}"
+		else
+			return arr.sum
+		end
 	end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -6,8 +6,20 @@ RSpec.describe StringCalculator do
 	subject(:str_cal_obj) { described_class.new }
 
 	describe "add(str) - calculate sum" do
-		it 'if input str is empty' do
-      expect(str_cal_obj.add("")).to eq(0)
-    end
+		context "passed cases" do
+			it 'if input str is empty' do
+	      expect(str_cal_obj.add("")).to eq(0)
+	    end
+
+	    it 'if input str contains \n with numbers' do
+	    	expect(str_cal_obj.add("1\n2,3")).to eq(6)
+	    end
+	  end
+
+    context "failed cases" do
+	    it 'if input str contains \n with negative numbers' do
+	    	expect{str_cal_obj.add("1\n2,-3")}.to raise_error("negative numbers not allowed -3")
+	    end
+	  end
 	end
 end
